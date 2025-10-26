f56d508e4d11ef26b2c4ef00f8da079990983ad2
musthafa <beingmusthafa48@gmail.com>
2025-10-26 16:37:56 +0530

    list delete and create

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import elasticClient from "./elastic.js";
+import cors from "cors";
 const app = express();
 
 app.use(
   cors({
     origin: "*",
-  })
+  }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,9 +15,20 @@ app.get("/", async (req, res) => {
   const entries = await elasticClient.search({
     index: "articles",
     query: {
+      match_all: {},
+    },
+  });
+  const results = entries.hits.hits.map((hit) => hit._source);
+  res.json(results);
+});
+
+app.get("/search/:title", async (req, res) => {
+  const entries = await elasticClient.search({
+    index: "articles",
+    query: {
       fuzzy: {
         title: {
-          value: req.query.title,
+          value: req.params.title,
           fuzziness: "AUTO",
         },
       },
@@ -29,12 +41,27 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   const { title, content } = req.body;
-  const article = { title, content };
+  const id = Math.random().toString(36).substring(2, 9);
+  const article = { title, content, id };
+  console.log("received create req : ", title);
   const response = await elasticClient.index({
     index: "articles",
     document: article,
   });
   res.status(201).json(response);
+});
+
+app.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const response = await elasticClient.deleteByQuery({
+    index: "articles",
+    query: {
+      match: {
+        id,
+      },
+    },
+  });
+  res.status(200).json(response);
 });
 
 app.listen(3000, () => {
